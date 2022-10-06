c0e4b5d3f9f317d393596bef85aef20982cf79aa
Chingun <97659944+solac911@users.noreply.github.com>
2022-10-06 20:25:44 +0800

    added mint event page & fixed organizer form

--- app/src/pages/MintEvent.jsx
+++ app/src/pages/MintEvent.jsx
@@ -1,14 +1,108 @@
 import React from "react";
 import { useState } from "react";
+import Switch from "react-switch";
 
 export default function MintEvent() {
-  const [eventName, setEventName] = useState();
-  const [eventDescription, setDescription] = useState();
-  const [eventLocation, setLocation] = useState();
-  const [eventDate, setDate] = useState();
-  const [eventSocialLink, setSocialLink] = useState();
-  const [eventAdmin, setAdmin] = useState();
-  const [ipfsUrl, setIpfsUrl] = useState();
+  const [state, setState] = useState([]);
 
-  return <div className="flex w-full justify-center"></div>;
+  function handleChange(e) {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit() {
+    console.log("state: ", state);
+  }
+
+  return (
+    <div className="w-full">
+      <div className="mt-10">
+        <div className="h-[7em] flex flex-col items-center justify-center font-body text-white space-y-6">
+          <span className="text-3xl font-bold text-black">Create Event</span>
+          <p className="text-lg text-black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            repellat.
+          </p>
+        </div>
+        <div className="xl:container mx-auto xl:px-20 md:px-12 px-4 font-body">
+          <div className="w-full sm:w-2/3 md:w-1/2 lg:w-2/5 mx-auto border rounded-2xl my-5 p-6 space-y-6">
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event name</p>
+              <input
+                type="text"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventName"
+                placeholder="DeFi Night"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event description</p>
+              <input
+                type="text"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventDescription"
+                placeholder="ArdMoney launchpad with community members"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event date</p>
+              <input
+                type="date"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventDate"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event location</p>
+              <input
+                type="text"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventLocation"
+                placeholder="Central tower, Sukhbaatar District 14200 Ulaanbaatar, Mongolia"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">
+                Enter your event organizer social link
+              </p>
+              <input
+                type="url"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventSocialLink"
+                placeholder="https://www.facebook.com/"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event admin address</p>
+              <input
+                type="text"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="eventAdmin"
+                placeholder="0x512...B0"
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your event nft ipfs url</p>
+              <input
+                type="url"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                onChange={handleChange}
+                name="ipfsUrl"
+                placeholder="https://ipfs.tech/"
+              />
+            </div>
+
+            <div className="w-full flex justify-center items-center rounded bg-blue-500 hover:bg-blue-600 cursor-pointer h-10 font-semibold text-white">
+              <button onClick={() => {}}>Mint Event NFT</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/src/pages/OrganizerForm.jsx
+++ app/src/pages/OrganizerForm.jsx
@@ -1,59 +1,54 @@
 import React from "react";
 import { useState } from "react";
 
-export default function OrganizerForm() {
+export default function MintEvent() {
   const [userNameInput, setUserName] = useState("");
   const [userLinkedinInput, setLinkedin] = useState("");
   const [userEmailInput, setEmail] = useState("");
-
   return (
-    <div className="flex w-full justify-center">
-      <div>
-        <div className="flex max-w-3xl w-full mt-10 mb-5 justify-center">
-          <img
-            src="https://images.pexels.com/photos/1422032/pexels-photo-1422032.jpeg?cs=srgb&dl=pexels-jess-bailey-designs-1422032.jpg&fm=jpg"
-            alt=""
-          />
+    <div className="w-full">
+      <div className="mt-10">
+        <div className="h-[7em] flex flex-col items-center justify-center font-body text-white space-y-6">
+          <span className="text-3xl font-bold text-black">
+            Become Event Organizer
+          </span>
+
+          <p className="text-lg text-black">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            repellat.
+          </p>
         </div>
-        <div className="pt-5">
-          <span className="font-bold text-4xl">Become Event Organizer</span>
-          <br />
-          <div className="border rounded-2xl my-5 p-6">
-            <span className="font-semibold text-2xl">Enter your name:</span>{" "}
-            <br />
-            <input
-              type="text"
-              className="border rounded w-full h-9 my-2"
-              placeholder=" Chingun"
-              onChange={(e) => {
-                setUserName(e.target.value);
-              }}
-              value={userNameInput}
-            />
-            <span className="font-semibold text-2xl">Enter your e-mail:</span>{" "}
-            <br />
-            <input
-              type="text"
-              className="border rounded w-full h-9 my-2"
-              placeholder=" chingunee.dev@gmail.com"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              value={userEmailInput}
-            />
-            <span className="font-semibold text-2xl">Enter your linkedin:</span>{" "}
-            <br />
-            <input
-              type="text"
-              className="border rounded w-full h-9 my-2"
-              placeholder=" www.linkedin.com/in/chingunee"
-              onChange={(e) => {
-                setLinkedin(e.target.value);
-              }}
-              value={userLinkedinInput}
-            />
-            <div className="flex justify-center text-white bg-blue-600 px-10 py-1 mt-2 border rounded-3xl hover:border-black w-3">
-              <button onClick={() => {}}>Mint</button>
+        <div className="xl:container mx-auto xl:px-20 md:px-12 px-4 font-body">
+          <div className="w-full sm:w-2/3 md:w-1/2 lg:w-2/5 mx-auto border rounded-2xl my-5 p-6 space-y-6">
+            <div className="space-y-2">
+              <p className="font-medium">Enter your name</p>
+              <input
+                type="text"
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                placeholder="Chingun Amarbaatar"
+                onChange={(e) => {}}
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your e-mail</p>
+              <input
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                type="email"
+                placeholder="chingunee.dev@gmail.com"
+                onChange={(e) => {}}
+              />
+            </div>
+            <div className="space-y-2">
+              <p className="font-medium">Enter your name</p>
+              <input
+                className="border rounded w-full h-12 px-3 focus:outline-none"
+                type="url"
+                placeholder="www.linkedin.com/in/chingunee"
+                onChange={(e) => {}}
+              />
+            </div>
+            <div className="w-full flex justify-center items-center rounded bg-blue-500 hover:bg-blue-600 cursor-pointer h-10 font-semibold text-white">
+              <button onClick={() => {}}>Mint Event Organizer NFT</button>
             </div>
           </div>
         </div>
