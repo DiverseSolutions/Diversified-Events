52a79f67c5745731acd4d4430926db508d4bae07
ccireedui <ireedui355@gmail.com>
2022-10-13 17:54:19 +0800

    Changed 'Join' to 'Mint'

--- app/src/components/EventCard.jsx
+++ app/src/components/EventCard.jsx
@@ -7,26 +7,26 @@ const Card = (props) => {
 
   return (
     <div
-      className='flex flex-col justify-start text-center border rounded-2xl h-60 p-3 hover:border-black cursor-pointer select-none'
+      className="flex flex-col justify-start text-center border rounded-2xl h-60 p-3 hover:border-black cursor-pointer select-none"
       onClick={() => navigate("/event-detail")}
     >
-      <div className='w-full flex justify-center'>
+      <div className="w-full flex justify-center">
         <img
           src={data.logo}
           width={"80px"}
           height={"80px"}
-          alt=''
-          className='flex justify-center text-center'
+          alt=""
+          className="flex justify-center text-center"
         />
       </div>
-      <span className='font-medium text-xs'>{data.date}</span>
-      <span className='text-lg font-semibold'>{data.name}</span>
-      <span className='font-medium text-gray-500 hover:underline cursor-pointer'>
+      <span className="font-medium text-xs">{data.date}</span>
+      <span className="text-lg font-semibold">{data.name}</span>
+      <span className="font-medium text-gray-500 hover:underline cursor-pointer">
         {data.organizer}
       </span>
-      <div className='flex justify-center mt-2 items-center'>
-        <button className='flex justify-center px-10 py-1 border rounded-3xl hover:border-black'>
-          Join
+      <div className="flex justify-center mt-2 items-center">
+        <button className="flex justify-center px-10 py-1 border rounded-3xl hover:border-black">
+          Mint
         </button>
       </div>
     </div>
