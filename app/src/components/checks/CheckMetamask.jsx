9d161f335de001f5b5aa35ad49fb3143a95ae458
erkhes1207 <ankhbayarerkhes@gmail.com>
2022-10-14 11:26:03 +0800

    added check of isConnected

--- app/src/components/checks/CheckMetamask.jsx
+++ app/src/components/checks/CheckMetamask.jsx
@@ -6,6 +6,7 @@ import {
   connectMetamask,
 } from "../../slices/metamaskSlice";
 import Metamask from "../../../assets/metamask.png";
+import Layout from "../../components/Layout";
 
 const CheckMetamask = ({ children }) => {
   const metamask = useSelector((state) => state.metamask);
@@ -31,6 +32,25 @@ const CheckMetamask = ({ children }) => {
           No metamask your browser, <br></br> Please download have metamask
           browser
         </span>
+      </div>
+    );
+  }
+
+  if (!metamask.isConnected) {
+    return (
+      <div className='flex flex-col items-center justify-center w-full h-screen'>
+        <img src={Metamask} width={200} height={200} alt='Metamask Image' />
+        <span className='text-xl font-bold text-center text-black md:text-2xl mt-5'>
+          Please connect Metamask
+        </span>
+        <button
+          className='text-md font-bold text-center text-white md:text-md py-2 px-5 mt-1 bg-blue-400 rounded-2xl'
+          onClick={() => {
+            dispatch(connectMetamask());
+          }}
+        >
+          Connect Metamask
+        </button>
       </div>
     );
   }
